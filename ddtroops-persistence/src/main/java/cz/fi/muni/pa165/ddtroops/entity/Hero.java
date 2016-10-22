51b495f853f0344951c940bf202977bc1584a545
Pepo48 <pepo48@gmail.com>
2016-10-22 14:53:54 +0200

    Added HeroDAO
    Signed-off-by: Pepo48 <pepo48@gmail.com>

--- ddtroops-persistence/src/main/java/cz/fi/muni/pa165/ddtroops/entity/Hero.java
+++ ddtroops-persistence/src/main/java/cz/fi/muni/pa165/ddtroops/entity/Hero.java
@@ -59,7 +59,8 @@ public class Hero {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+
+        if (o == null || !(o instanceof Hero)) return false;
 
         Hero hero = (Hero) o;
 
