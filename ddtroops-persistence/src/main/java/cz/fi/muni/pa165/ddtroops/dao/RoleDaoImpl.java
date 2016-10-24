79c446f3a6bb270efea3413f86ef4a944f8a474e
Petr Koláček <kolacek@reinto.cz>
2016-10-24 22:20:52 +0200

    Role DAO

--- ddtroops-persistence/src/main/java/cz/fi/muni/pa165/ddtroops/dao/RoleDaoImpl.java
+++ ddtroops-persistence/src/main/java/cz/fi/muni/pa165/ddtroops/dao/RoleDaoImpl.java
@@ -5,37 +5,65 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.NoResultException;
 
 /**
  * Created by pstanko.
  *
- * @author pstanko
+ * @author P. Kolacek
  */
 @Repository
 @Transactional
 public class RoleDaoImpl implements RoleDao {
+    
+    @PersistenceContext
+    private EntityManager em;
+    
     @Override
-    public Role create(Role group) {
-        return null;
+    public Role create(Role role)
+    {
+        em.persist(role);
+        return role;
     }
 
     @Override
-    public Role delete(Role group) {
-        return null;
+    public Role delete(Role role)
+    {
+        em.remove(em.contains(role) ? role : em.merge(role));
+        return role;
     }
 
     @Override
-    public Role update(Role group) {
-        return null;
+    public Role update(Role role)
+    {
+        role = em.merge(role);
+        return role;
     }
 
     @Override
-    public Role getById(long id) {
-        return null;
+    public Role findById(long id)
+    {
+        return em.find(Role.class, id);
+    }
+    
+    @Override
+    public Role findByName(String name)
+    {
+        try{
+            return em.createQuery("select r from Role r where name = :name", Role.class)
+                    .setParameter("name", name)
+                    .getSingleResult();
+        }catch(NoResultException e){
+            return null;
+        }
     }
 
     @Override
-    public List<Role> listAll() {
-        return null;
+    public List<Role> listAll()
+    {
+        return em.createQuery("select r from Role r", Role.class)
+                .getResultList();
     }
 }
--- ddtroops-persistence/src/main/java/cz/fi/muni/pa165/ddtroops/entity/Role.java
+++ ddtroops-persistence/src/main/java/cz/fi/muni/pa165/ddtroops/entity/Role.java
@@ -48,5 +48,30 @@ public class Role {
         this.description = name;
     }
     
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Role))
+            return false;
+        Role role = (Role) obj;
+        if (name == null) {
+            if (role.getName() != null)
+                return false;
+        } else if (!name.equals(role.getName()))
+            return false;
+        return true;
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 37;
+        int result = 1;
+        result = prime * result + ((getName() == null) ? 0 : getName().hashCode());
+        return result;
+    }
+    
     
 }
