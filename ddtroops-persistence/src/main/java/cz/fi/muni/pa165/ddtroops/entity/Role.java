6ff2a5866f8dfe9f79839382394a906c3aed85c8
Petr Koláček <kolacek@reinto.cz>
2016-10-24 21:35:25 +0200

    Role entity properties, methods

--- ddtroops-persistence/src/main/java/cz/fi/muni/pa165/ddtroops/entity/Role.java
+++ ddtroops-persistence/src/main/java/cz/fi/muni/pa165/ddtroops/entity/Role.java
@@ -1,12 +1,14 @@
 package cz.fi.muni.pa165.ddtroops.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 /**
- * @author
+ * @author P. Kolacek <xkolac11>
  */
 @Entity
 public class Role {
@@ -14,4 +16,37 @@ public class Role {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
+    
+    @NotNull
+    @Column(nullable=false,unique=true)
+    private String name;
+    
+    @Column(nullable = false)
+    private String description;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String name) {
+        this.description = name;
+    }
+    
+    
 }
